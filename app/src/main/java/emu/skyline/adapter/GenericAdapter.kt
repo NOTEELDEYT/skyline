f1b57c5ab54f8735a26fc053846e2e2a3ef873cb
lynxnb <niccolo.betto@gmail.com>
2022-07-13 16:35:26 +0200

    Show an undo button after deleting a gpu driver
    After a driver has been deleted, a snackbar will be shown confirming the deletion, with an button to undo it.

--- app/src/main/java/emu/skyline/adapter/GenericAdapter.kt
+++ app/src/main/java/emu/skyline/adapter/GenericAdapter.kt
@@ -73,6 +73,11 @@ open class GenericAdapter : RecyclerView.Adapter<GenericViewHolder<ViewBinding>>
         filter.filter(currentSearchTerm)
     }
 
+    open fun addItemAt(position : Int, item : GenericListItem<out ViewBinding>) {
+        allItems.add(position, item)
+        filter.filter(currentSearchTerm)
+    }
+
     fun setOnFilterPublishedListener(listener : OnFilterPublishedListener) {
         onFilterPublishedListener = listener
     }
@@ -152,7 +157,7 @@ class SelectableGenericAdapter(var defaultPosition : Int) : GenericAdapter() {
             notifyItemChanged(position)
         }
 
-    override fun removeItemAt(position: Int) {
+    override fun removeItemAt(position : Int) {
         super.removeItemAt(position)
         if (position == selectedPosition)
             selectedPosition = defaultPosition
